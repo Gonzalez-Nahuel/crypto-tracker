7cc89924f4b9f7090262b112c04b03a69124e84c
nahuel <nahuelg976@gmail.com>
2026-05-30 20:41:18 -0300

    feat(ci): add signup-route itegration test to CI workflow

--- src/test/integration/signup-route.integration.test.ts
+++ src/test/integration/signup-route.integration.test.ts
@@ -1,4 +1,3 @@
-import { POST } from "@/app/api/auth/signup/route";
 import { prisma } from "@/lib/db/prisma";
 import { describe, it, expect, vi } from "vitest";
 
@@ -8,20 +7,21 @@ vi.mock("@/lib/auth/create-verification-token", () => ({
 
 describe("POST /api/auth/signup", () => {
   it("should create a user", async () => {
-    const req = new Request("http://localhost:3000/api/auth/signup", {
+    const res = await fetch("http://localhost:3000/api/auth/signup", {
       method: "POST",
       body: JSON.stringify({
         username: "test",
         email: "test@test.com",
         password: "Wdfeaaf12#",
       }),
+      headers: {
+        "Content-Type": "application/json",
+      },
     });
 
-    const response = await POST(req);
+    const data = await res.json();
 
-    const data = await response.json();
-
-    expect(response.status).toBe(201);
+    expect(res.status).toBe(201);
     expect(data.ok).toBe(true);
 
     const user = await prisma.user.findUnique({
@@ -36,37 +36,39 @@ describe("POST /api/auth/signup", () => {
   });
 
   it("should return status 400, missing credentials", async () => {
-    const req = new Request("http://localhost:3000/api/auth/signup", {
+    const res = await fetch("http://localhost:3000/api/auth/signup", {
       method: "POST",
       body: JSON.stringify({
         username: "test",
         email: "test@test.com",
       }),
+      headers: {
+        "Content-Type": "application/json",
+      },
     });
 
-    const response = await POST(req);
+    const data = await res.json();
 
-    const data = await response.json();
-
-    expect(response.status).toBe(400);
+    expect(res.status).toBe(400);
     expect(data.message).toBe("Missing credentials");
   });
 
   it("should return status: 500, ok: false", async () => {
-    const req = new Request("http://localhost:3000/api/auth/signup", {
+    const res = await fetch("http://localhost:3000/api/auth/signup", {
       method: "POST",
       body: JSON.stringify({
         username: "test",
         email: "testtest.com",
         password: "Wlñlams154%",
       }),
+      headers: {
+        "Content-Type": "application/json",
+      },
     });
 
-    const response = await POST(req);
+    const data = await res.json();
 
-    const data = await response.json();
-
-    expect(response.status).toBe(422);
+    expect(res.status).toBe(422);
     expect(data.message).toBe("Correo electronico inválido");
   });
 });
